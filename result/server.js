cb42dcb69bb2908231f471b9f03b5630a030eba1
howlcart <imannuelsmkn1bms@gmail.com>
2025-04-08 10:28:58 +0700

    Main Stage Update Connection App

--- result/server.js
+++ result/server.js
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: 'postgres://postgres:LKSN2024@lks-rdss.c5k659fmsxgw.us-east-1.rds.amazonaws.com:5432/postgres'
 });
 
 async.retry(
