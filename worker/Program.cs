ececfbd9866f3bc2b05e88b24d24b9ff832d9c63
howlcart <imannuelsmkn1bms@gmail.com>
2025-04-09 09:55:26 +0700

    Main Stage Rebuild Worker Img

--- worker/Program.cs
+++ worker/Program.cs
@@ -34,7 +34,7 @@ namespace Worker
                     // Reconnect redis if down
                     if (redisConn == null || !redisConn.IsConnected) {
                         Console.WriteLine("Reconnecting Redis");
-                        redisConn = OpenRedisConnection("redis");
+                        redisConn = OpenRedisConnection("master.lks-redis.3qhbn9.use1.cache.amazonaws.com:6379");
                         redis = redisConn.GetDatabase();
                     }
                     string json = redis.ListLeftPopAsync("votes").Result;
